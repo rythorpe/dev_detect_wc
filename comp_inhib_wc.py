fix jacobian

comp_inhib_wc.py
@@ -30,7 +30,8 @@ def dxdt_v2(x, w, inj_excite, tau=20.0, tau_2=None, thresh=0.5, thresh_2=None,
     return x_
 
 
-def jacobian(x, w, thresh, thresh_2, steepness, steepness_2, inj_excite=0):
+def jacobian(x, w, tau, tau_2, thresh, thresh_2, steepness, steepness_2,
+             inj_excite=0):
     """Jacobian of Wilson-Cowan ODEs (dxdt_v2)."""
     # NB: here, n_dim represents full rank of system (i.e., total number of
     # neural mass units across layers)
@@ -38,23 +39,25 @@ def jacobian(x, w, thresh, thresh_2, steepness, steepness_2, inj_excite=0):
     x = np.array(x)  # convert to array if not one already
     g = (x @ w) + inj_excite
     J = np.zeros((n_dim, n_dim))
-    for i_idx in range(n_dim):
-        # contribution from upper layer (units 0-1)
-        for j_idx in range(n_dim // 2):
-            sig = sigmoid(g[i_idx], thresh, steepness)
+    for i_idx in range(n_dim // 2):
+        # upper layer (units 0-1)
+        sig = sigmoid(g[i_idx], thresh, steepness)
+        for j_idx in range(n_dim):
             dfdx = w[i_idx, j_idx] * steepness * (1 / sig - 1) * sig ** 2
             if i_idx == j_idx:
-                J[i_idx, j_idx] = -x[i_idx] + dfdx
+                J[i_idx, j_idx] = (-1 + dfdx) / tau
             else:
-                J[i_idx, j_idx] = dfdx
-        # contribution from lower layer (units 2-3)
-        for j_idx in range(n_dim // 2, n_dim):
-            sig = sigmoid(g[i_idx], thresh_2, steepness_2)
+                J[i_idx, j_idx] = dfdx / tau
+
+    for i_idx in range(n_dim // 2, n_dim):
+        # lower layer (units 2-3)
+        sig = sigmoid(g[i_idx], thresh_2, steepness_2)
+        for j_idx in range(n_dim):
             dfdx = w[i_idx, j_idx] * steepness_2 * (1 / sig - 1) * sig ** 2
             if i_idx == j_idx:
-                J[i_idx, j_idx] = -x[i_idx] + dfdx
+                J[i_idx, j_idx] = (-1 + dfdx) / tau_2
             else:
-                J[i_idx, j_idx] = dfdx
+                J[i_idx, j_idx] = dfdx / tau_2
     return J
 
 